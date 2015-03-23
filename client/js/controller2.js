409447133edfc31d7ec546c7dfd1efbbefaafd97
kioku <kentaro-o@digirea.com>
2015-03-23 23:59:35 +0900

    separate from bottomtab

--- client/js/controller2.js
+++ client/js/controller2.js
@@ -2040,9 +2040,18 @@
 					'leftArea' : { min : '0px', max : '250px' },
 				}, 'Contents'
 			),
-			bottomfunc = window.animtab.create('bottom',
+			/*bottomfunc = window.animtab.create('bottom',
 				{ 'bottomTab' : { min : '0px', max : 'auto' }},
-				{ 'bottomArea' : { min : '0px', max : '400px' }}, 'AddContent'),
+				{ 'bottomArea' : { min : '0px', max : '400px' }}, 'AddContent'),*/
+			bottomfunc = function (show) {
+				if (show) {
+					$show($('overall_block'));
+					$show($('bottomArea'));
+				} else {
+					$hide($('overall_block'));
+					$hide($('bottomArea'));
+				}
+			},
 			rightfunc = window.animtab.create('right',
 				{ 'rightTab' : { min : '0px', max : 'auto' }},
 				{ 'rightArea' : { min : '0px', max : '250px' }}, 'Property');
@@ -2105,6 +2114,9 @@
 			if (lastDraggingID && !manipulator.getDraggingManip()) {
 				unselect();
 			}
+		});
+		document.getElementById('overall_block').addEventListener('click', function (evt) {
+			bottomfunc(false);
 		});
 		
 		updateScreen();
